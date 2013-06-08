01f0c3663b4d12cf72271817c2e261d4c1bba6a9
Antonin Hildebrand <antonin@hildebrand.cz>
2013-06-08 12:21:32 -0700

    don't pipe git output to stdout
    and more robust updating of git repo

--- lib/prism/dwarfs.rb
+++ lib/prism/dwarfs.rb
@@ -25,24 +25,35 @@ def Prism.archive_pull_url()
   "#{@config[:token]}@github.com:binaryage/totalfinder-archive.git"
 end
 
+def Prism.exec(cmd)
+  res = `#{cmd} 2>&1`
+  unless $?.success?
+    puts res
+    die "failed: #{cmd}"
+  end
+  res
+end
+
 def Prism.download_dwarfs(version)
   ensure_existence_of_dwarfs_cache_dir()
   FileUtils.mkdir_p(work_dir())
   Dir.chdir(work_dir()) do
     unless File.exists?("totalfinder-archive")
       # see https://github.com/blog/1270-easier-builds-and-deployments-using-git-over-https-and-oauth
-      # don't do system("git clone --recursive git@github.com:binaryage/totalfinder-archive.git")
-      FileUtils.mkdir("totalfinder-archive")
-      Dir.chdir("totalfinder-archive") do
-        system("git init")
-        system("git pull #{archive_pull_url()}")
+      # don't do exec("git clone --recursive git@github.com:binaryage/totalfinder-archive.git")
+      FileUtils.rm_rf("totalfinder-archive-test")
+      FileUtils.mkdir("totalfinder-archive-test")
+      Dir.chdir("totalfinder-archive-test") do
+        exec("git init")
+        exec("git pull #{archive_pull_url()}")
       end
+      exec("mv totalfinder-archive-test totalfinder-archive")
     end
     Dir.chdir("totalfinder-archive") do
       # reset & update
-      system("git checkout master")
-      system("git reset --hard HEAD")
-      system("git pull #{archive_pull_url()}")
+      exec("git clean -f -f -d") # http://stackoverflow.com/questions/9314365/git-clean-is-not-removing-a-submodule-added-to-a-branch-when-switching-branches
+      exec("git reset --hard HEAD^") # use previous commit to make working tree resilient to amends
+      exec("git pull --ff-only \"#{archive_pull_url()}\"")
 
       # find revision with our version
       rev = `git log --grep="#{version}" -n 1`
@@ -51,7 +62,7 @@ def Prism.download_dwarfs(version)
       die "failed to retrieve commit of dwarfs version #{version}" if commit.empty?
 
       # checkout our version
-      system("git checkout #{commit}")
+      exec("git checkout #{commit}")
 
       # copy dwarfs into cache (under version subfolder)
       dwarfs_path = dwarfs_path_in_cache(version)
