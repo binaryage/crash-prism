94e52e8eaa8afae3cd4d656a9b81f85ac28719bb
Antonin Hildebrand <antonin@hildebrand.cz>
2016-03-12 19:06:40 +0100

    make prism product agnostic and follow new tagging conventions

--- lib/prism.rb
+++ lib/prism.rb
@@ -2,7 +2,8 @@ module Prism
   VERSION = "0.2.0"
   @config = {
     :workspace => "/tmp/crash-prism",
-    :token => nil
+    :repo => "github.com:binaryage/root.git", # you might need to specify token: https://
+    :product => "TotalFinder"
   }
   class << self; attr_accessor :config; end # an ugly way how to expose Prism.config to outside world
 end
--- lib/prism/dwarfs.rb
+++ lib/prism/dwarfs.rb
@@ -33,9 +33,32 @@ def Prism.dwarfs_exist?(version)
   File.exists?(dwarfs_path)
 end
 
-def Prism.archive_pull_url()
-  return "git@github.com:binaryage/totalfinder-archive.git" unless @config[:token]
-  "https://#{@config[:token]}@github.com/binaryage/totalfinder-archive.git"
+# each product has own archive branch in the repo
+# by convention PRODUCT-archive
+# for example totalfinder-archive 
+# or totalterminal-archive
+# https://github.com/binaryage/root/tree/totalfinder-archive
+def Prism.product_branch()
+  @config[:product].downcase + "-archive"
+end
+
+# by convention product tags look like
+#   totalfinder-archive-v1.7.7
+#   totalfinder-archive-v1.7.8
+#   totalfinder-archive-v1.7.9
+#   totalfinder-archive-v1.7.10
+#   totalfinder-archive-v1.7.11
+#   totalfinder-archive-v1.7.12
+# 
+#   or 
+#
+#   totalterminal-archive-v1.4.11
+#   totalterminal-archive-v1.5
+#   totalterminal-archive-v1.5.4
+#   totalterminal-archive-v1.6
+#
+def Prism.product_tag_prefix()
+  @config[:product].downcase + "-archive"
 end
 
 def Prism.exec(cmd)
@@ -57,24 +80,28 @@ end
 def Prism.update_archive()
   ensure_existence_of_dwarfs_cache_dir()
   FileUtils.mkdir_p(work_dir())
-  puts "workdir '#{work_dir()}'"
   Dir.chdir(work_dir()) do
-    unless File.exists?("totalfinder-archive")
+    branch = product_branch()
+    repo = @config[:repo]
+    unless File.exists?(branch)
       # see https://github.com/blog/1270-easier-builds-and-deployments-using-git-over-https-and-oauth
       # don't do exec("git clone --recursive git@github.com:binaryage/totalfinder-archive.git")
-      FileUtils.rm_rf("totalfinder-archive-test")
-      FileUtils.mkdir("totalfinder-archive-test")
-      Dir.chdir("totalfinder-archive-test") do
-        exec("git init")
-        exec("git pull #{archive_pull_url()}")
+      temp_folder = branch + "-test"
+      FileUtils.rm_rf(temp_folder)
+      cmd = "git clone -b \"#{branch}\" \"#{repo}\" \"#{temp_folder}\""
+      puts "> #{cmd.yellow}"
+      system(cmd) # this can take a long time, give user feedback
+      unless $?.success?
+        puts res
+        die "failed: #{cmd}"
       end
-      exec("mv totalfinder-archive-test totalfinder-archive")
+      exec("mv \"#{temp_folder}\" \"#{branch}\"")
     end
-    Dir.chdir("totalfinder-archive") do
+    Dir.chdir(branch) do
       # reset & update
       exec("git clean -f -f -d") # http://stackoverflow.com/questions/9314365/git-clean-is-not-removing-a-submodule-added-to-a-branch-when-switching-branches
       exec("git reset --hard HEAD^") # use previous commit to make working tree resilient to amends
-      exec("git pull --ff-only \"#{archive_pull_url()}\"")
+      exec("git pull --ff-only \"#{repo}\"")
     end
   end
 end
@@ -82,10 +109,12 @@ end
 def Prism.download_dwarfs(version)
   update_archive()
   Dir.chdir(work_dir()) do
-    Dir.chdir("totalfinder-archive") do
+    branch = product_branch()
+    Dir.chdir(branch) do
       # find revision with our version
-      rev = exec("git log --grep=\"#{version}\" -n 1 --format=oneline")
-      die "unable to find archive commit with #{version}" if rev.nil?
+      tag_matcher = "#{product_tag_prefix()}-v#{version}}"
+      rev = exec("git log --grep=\"#{tag_matcher}\" -n 1 --format=oneline")
+      die "unable to find archive commit with #{tag_matcher}" if rev.nil?
       commit = rev.split("\n")[0].split(" ")[0].strip
 
       die "failed to retrieve commit of dwarfs version #{version}" if commit.empty?
